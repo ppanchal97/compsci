fd83a5a63a114d0c8563f3bee1a3a0990ddfaa06
ParikPanchal <parik.panchal@bcgdv.com>
2021-03-22 13:10:09 +0000

    22-03-21

--- fibonacci_recursive.py
+++ fibonacci_recursive.py
@@ -1,9 +1,8 @@
 def fibonacci(n):
     # base case
-    if n == 0:
+    if n == 1 or n == 0:
         return 1
     else:
-        return fibonacci(n -1) + fibonacci(n - 2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
-n = int(input())
-print(fibonacci(n))
+print(fibonacci(3))
